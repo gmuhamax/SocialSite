ec20f41cb7273be7c826316c771d5c2a0fc7a629
daniil <daniilnaryzniak@gmail.com>
2024-05-24 13:45:16 +0300

    create Post and comment models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from account.models import User
 
+
+class Post (models.Model):
+    preview = models.ImageField()
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    dislike = models.IntegerField()
+    like = models.IntegerField()
+    created = models.DateField(auto_now_add=True)
+    Comments = models.ForeignKey(User,
+                                 on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(Post,
+                                on_delete=models.CASCADE)
+    body = models.CharField(max_length=200)
